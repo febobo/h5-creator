a1411f8f52a761e1fed64c0554cb78dc1ea4ac8a
febobo <febobo@zhangbodeMacBook-Pro.local>
2016-08-10 15:38:33 +0800

    add gulpfile on auto build

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
-
+var $ = require('gulp-load-plugins')();
 //Server
 gulp.task('server', function() {
 	connect.server({
     name: 'Dev App',
-    root: 'src/page',
+    root: ['./','src/.tmp'],
     port: 8000,
     livereload: true,
 		open : true,
   });
 });
 
-gulp.task('html', function () {
+gulp.task('stylus',['inject'], function () {
+  return gulp.src('./src/css/*.styl')
+    .pipe($.stylus())
+    .pipe(gulp.dest('./src/css/build'))
+		.pipe(connect.reload())
+});
+
+gulp.task('html', ['inject'] , function () {
   gulp.src('./src/page/*.html')
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
   gulp.watch(['./src/page/*.html'], ['html']);
+	gulp.watch(['./src/css/*.styl'], ['stylus']);
 });
 
-gulp.task('serve',['server','watch'])
+gulp.task('inject' , function(){
+	var target = gulp.src('./src/page/index.html');
+  var sources = gulp.src(['./src/**/**/*.css'], {read: false});
+  return target.pipe($.inject(sources))
+    .pipe(gulp.dest('./src/.tmp/'));
+})
+
+gulp.task('serve',['server','watch','stylus','inject'])
