20fcc7397afc729420ba673870be9de531877cb7
febobo <febobo@zhangbodeMacBook-Pro.local>
2016-08-11 17:00:00 +0800

    index layout

--- gulpfile.js
+++ gulpfile.js
@@ -24,14 +24,20 @@ gulp.task('html', ['inject'] , function () {
     .pipe(connect.reload());
 });
 
+gulp.task('script', ['inject'] , function () {
+  gulp.src('./src/js/*.js')
+    .pipe(connect.reload());
+});
+
 gulp.task('watch', function () {
   gulp.watch(['./src/page/*.html'], ['html']);
 	gulp.watch(['./src/css/*.styl'], ['stylus']);
+	gulp.watch(['./src/js/*.js'], ['script']);
 });
 
 gulp.task('inject' , function(){
 	var target = gulp.src('./src/page/index.html');
-  var sources = gulp.src(['./src/**/**/*.css'], {read: false});
+  var sources = gulp.src(['./src/**/**/*.css','./src/**/*.js'], {read: false});
   return target.pipe($.inject(sources))
     .pipe(gulp.dest('./src/.tmp/'));
 })
