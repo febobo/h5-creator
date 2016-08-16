f83084e7d10762fe16bc9545f65862e6151b9908
febobo <febobo@zhangbodeMacBook-Pro.local>
2016-08-16 15:36:40 +0800

    index layout

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,31 @@ var utils = {
 				return $(v)
 			}
 		})
+	},
+	toPercent : function(origin,num){
+		return (num/origin).toFixed(2) * 100 + '%';
+	},
+	percentTo : function(origin,num){
+		return origin.replace(/[%]/g,'')/100;
+	},
+	storage : {
+		get: function(name) {
+			var res = localStorage.getItem(name);
+			if(res) {
+				return JSON.parse(res);
+			} else {
+				return null;
+			}
+		},
+		set: function(name, jsonObj) {
+			localStorage.setItem(name, JSON.stringify(jsonObj));
+		},
+		remove: function(name) {
+			localStorage.removeItem(name);
+		}
 	}
 }
+utils.toPercent(254,25)
 // 记录最后一次操作的元素
 var lastTarget;
 Fill = function(type, ele) {
@@ -54,7 +77,6 @@ function addOnlyKey(ele,key){
 
 // 将选中的元素样式添加至可视区
 function textView (ele){
-
 	var styles = utils.getCss(ele[0]),
 		text = ele.text();
 	$('#eleText').attr('value',text);
@@ -84,23 +106,46 @@ function textView (ele){
 	})
 }
 
+// 将选中的元素样式添加至可视区
+function imageView (ele){
+	var styles = utils.getCss($(ele)[0]);
+	addOnlyKey(['#eleUrl','#eleAlign'],$(ele).attr('key'))
+
+	var percent = utils.toPercent($(ele).width() , $(ele).children('img').width())
+
+	$('#imageWidth').val(percent)
+	$('#imageHeight').val($(ele).children('img').height())
+	$.each($('#imageAlign > *'),function(k,v){
+		if(styles.textAlign === 'start' ){
+			return $('#imageAlign button').eq(0).addClass('btn-success').siblings().removeClass('btn-success')
+		}
+		if($(v).attr('align') === styles.textAlign){
+			$(v).addClass('btn-success').siblings().removeClass('btn-success')
+		}
+	})
+}
+
 // 检查元素类型
 function checkType(type,ele){
-	if (type === 'text') {
-		textView(ele);
+	if (type === 'title') {
+		textView($(ele));
+		viewSwitch($('.typeSelect li').eq(0),type)
+  }
+	if (type === 'image') {
+		imageView(ele);
+		viewSwitch($('.typeSelect li').eq(1),type)
   }
 }
 
 $('.tmp_content > * > *').on('click', function() {
   $('.view_content').append($(this).clone());
-	lastTarget = $(this).clone();
-	checkType($(this).attr('type'),$(this))
+	// lastTarget = $(this).clone();
+	checkType($(this).attr('type'),this)
 })
 
 $('.view_content').on('click','*' ,function(evt) {
 	lastTarget = $(this);
-	console.log(lastTarget)
-	checkType($(this).attr('type'),$(this))
+	checkType($(this).attr('type'),this)
 })
 
 function createSizeOption(min,max){
@@ -151,7 +196,6 @@ var editTitle = {
 			var attr;
 			if($(this).hasClass('btn-success')){
 				attr = $(this).attr('align');
-				console.log(attr)
 			}
 			$(lastTarget).css({
 				textAlign : attr
@@ -161,14 +205,15 @@ var editTitle = {
 }
 
 
-// 标题编辑
+// 图片编辑
 var editImage = {
 	init : function(){
 		this.changeUrl();
+		this.changeSize();
+		this.changeAlign();
 	},
 	changeUrl : function(){
 		$('#eleUrl').on('change',function(evt){
-			console.log(222)
 			var file = this.files[0];
 	    if(!/image\/\w+/.test(file.type)){
 	        alert("文件必须为图片！");
@@ -177,22 +222,45 @@ var editImage = {
 	    var reader = new FileReader();
 	    reader.readAsDataURL(file);
 	    reader.onload = function(e){
-				$(lastTarget).attr('src',e.srcElement.result)
+				$(lastTarget).children('img').attr('src',e.srcElement.result)
 	    }
+		})
+	},
+	changeSize : function(){
+		$('#imageWidth').on('input propertychange',function(event){
+			$(lastTarget).children('img').css({
+				width : utils.percentTo($(this).val()) * $(lastTarget).width() + 'px',
+			})
+			$('#imageHeight').val($(lastTarget).children('img').height())
+		})
+
+		$('#imageHeight').on('input propertychange',function(event){
+			$(lastTarget).children('img').css({
+				height : $(this).val() + 'px',
+			})
+		})
+	},
+	changeAlign : function(){
+		$('#imageAlign button').on('click',function(evt){
+			$(this).addClass('btn-success').siblings().removeClass('btn-success');
+			var attr;
+			if($(this).hasClass('btn-success')){
+				attr = $(this).attr('align');
+			}
+			$(lastTarget).css({
+				textAlign : attr
+			})
 		})
 	}
 }
 
-
-$('#deleteComponent').on('click' , function(){
-	console.log(lastTarget)
+$('.deleteComponent').on('click' , function(){
 	$(lastTarget).remove();
 })
 
-// view 切换
-$('.typeSelect li').on('click' , function(){
-	var data = $(this).attr('data');
-	$(this).addClass('active').siblings().removeClass('active');
+// 点击元素改变视图
+var viewSwitch = function(light,data){
+	light.addClass('active').siblings().removeClass('active');
 	var activeSwitch = function(ele){
 		$.each($(ele), function(k,v){
 			if($(v).attr('data') == data){
@@ -202,14 +270,22 @@ $('.typeSelect li').on('click' , function(){
 	}
 	activeSwitch('.tmp_content >div')
 	activeSwitch('.edit_content >div')
+}
+
+// view 切换
+$('.typeSelect li').on('click' , function(){
+	var data = $(this).attr('data');
+	viewSwitch($(this),data)
 })
 
 $('#eleSize').html(createSizeOption(12,42))
 editImage.init();
 editTitle.init();
 
-
-
-
-
 var sortable = Sortable.create($('.view_content')[0]);
+
+$('#save').on('click', function(){
+	// console.log($('.view_content').html())
+	utils.storage.set('content',$('.view_content').html())
+})
+$('.view_content').html(utils.storage.get('content'));
